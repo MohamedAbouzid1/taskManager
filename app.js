8729da7e86fe078ee3a9d7682bbb340ce4539a39
Mohamed Abouzid <mohamed.abouzid@bioinfsys.uni-giessen.de>
2025-05-29 19:07:19 +0200

    added MongoDB

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+const connectDB = require('./db/connect')
+
+const port = 5000
+const tasks = require('./routes/tasks')
+
+// middleware
+app.use(express.json())
+
+// routes
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App')
+})
+
+app.use('/api/v1/tasks', tasks)
+//app.get('/api/v1/tasks') - get all tasks
+//app.post('/api/v1/tasks') - create a new task
+//app.get('/api/v1/tasks/:id') - get single task
+//app.patch('/api/v1/tasks/:id') - update task
+//app.delete('/api/v1/tasks/:id') - delete task
+
+const start = async () => {
+    try {
+        await connectDB()
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
